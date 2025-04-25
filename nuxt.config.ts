9024068a6ef1a6ba41f58a1c8cd6005886afe76a
lucassato1030 <lucassato1030@gmail.com>
2025-04-25 12:20:52 -0700

    remove comments in config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,3 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
@@ -18,16 +17,12 @@ export default defineNuxtConfig({
     },
   },
   
-  // Configure Nitro specifically for Netlify hosting
   nitro: {
     preset: 'netlify'
-    // Removed prerender config to avoid Windows path issues
   },
   
-  // Explicitly enable SSR
   ssr: true,
 
-  // Update route rules to make sure root path is properly handled
   routeRules: {
     '/**': { ssr: true }
   }
